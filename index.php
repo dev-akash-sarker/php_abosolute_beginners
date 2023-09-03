9c1b30a959259d04afb84b69289c3329239a39a9
dev-akash-sarker <akashsarker210@gmail.com>
2023-09-04 01:12:48 +0600

    all

--- index.php
+++ index.php
@@ -5,17 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<!-- when we wants to show any output then use on Php are -->
-<?php
-echo "hello world"; // in a display
-echo "<br>" ;
-print "hello earth"; // in a display
-
-$food = print('Cocacola');
-
-echo $food;
+<!-- php Data Types -->
+<?php 
+$name = "akash";
+// this is string data type
+// we can use "" or ''
+// both are string
 ?>
+
+<?php 
+$age = 10;
+// in javascript 10 is number but
+// in php 10 is not number but integer
+// any number are called are integer php
+?>
+<?php 
+$age = 32.2;
+echo $age;
+// this is not integer but this is flout
+// doshomik number holo float type
+?>
+<?php 
+$name = "akash";
+$age = 10;
+$point = 34.5;
+
+var_dump($name);
+"<br>";
+var_dump($age) . '<br>';
+// var_dump($point) . '<br>';
+// in javascript we use "type of" to find which type is it!
+// in php we use var_dumb() to find out the same 
+?>
+
+<?php 
+$religeon = true;
+var_dump($religeon);
+// this is called boolean 
+// eather True or False will be the output
+?>
+<?php 
+$fruite = array('apple', 'banna', 'plum');
+var_dump($fruite);
+// this is array
+$null = null;
+var_dump($null);
+// null boltei kono kisu pay nai! emn kisu
+?>
+<?php ?>
+<?php ?>
+<?php ?>
+
+<body>
+
 
 
 </body>
