3224357e2262b6e3c351f808de3fd0a155b05e5f
dev-akash-sarker <akashsarker210@gmail.com>
2023-09-04 01:22:51 +0600

    class phone object

--- index.php
+++ index.php
@@ -5,60 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<!-- php Data Types -->
+<body>
 <?php 
-$name = "akash";
-// this is string data type
-// we can use "" or ''
-// both are string
-?>
+class Phone {
+    var $model;
+    function phoneModel ($number) {
+        global $model;
+        $model = $number;
+        echo "This is $model <br>"; 
+    }
+}
 
-<?php 
-$age = 10;
-// in javascript 10 is number but
-// in php 10 is not number but integer
-// any number are called are integer php
+$apple = new Phone;
+$apple-> phoneModel('iphone 16');
+$samsung = new Phone;
+$samsung -> phoneModel('samsung note 12');
+// ekhane object holo class
+// upore jemn Phone holoe ekti object
 ?>
-<?php 
-$age = 32.2;
-echo $age;
-// this is not integer but this is flout
-// doshomik number holo float type
-?>
-<?php 
-$name = "akash";
-$age = 10;
-$point = 34.5;
-
-var_dump($name);
-"<br>";
-var_dump($age) . '<br>';
-// var_dump($point) . '<br>';
-// in javascript we use "type of" to find which type is it!
-// in php we use var_dumb() to find out the same 
-?>
-
-<?php 
-$religeon = true;
-var_dump($religeon);
-// this is called boolean 
-// eather True or False will be the output
-?>
-<?php 
-$fruite = array('apple', 'banna', 'plum');
-var_dump($fruite);
-// this is array
-$null = null;
-var_dump($null);
-// null boltei kono kisu pay nai! emn kisu
-?>
-<?php ?>
 <?php ?>
 <?php ?>
 
 <body>
-
-
-
 </body>
 </html>
