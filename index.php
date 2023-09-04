f551d009fb058c46d494108ba963200ae4a7648a
dev-akash-sarker <akashsarker210@gmail.com>
2023-09-04 17:05:09 +0600

    all

--- index.php
+++ index.php
@@ -7,37 +7,6 @@
 </head>
 <body>
 
-<?php 
-//while loop
-$x = 0;
-while($x <= 5) {
-    echo "The number is: $x <br>";
-    $x++;
-
-}
-
-?>
-<?php
-// do while loop
-echo "<br>";
-// $y = 2;
-// do {
-//     echo "The number is: $y <br>";
-//     $y++;
-// } while($x <= 5);
-
-// for loop
-
-// for($z = 10; $z <=50; $z++){
-//     echo "The number is: $z <br>";
-// }
-
-for($m = 10; $m <=50; $m+=5){
-    echo "The number is: $m <br>";
-}
-
-?>
-
 
 <body>
 </body>
