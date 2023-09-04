2edc25f06b1e2101d8c69d1d85abc7af7221cf51
dev-akash-sarker <akashsarker210@gmail.com>
2023-09-04 16:20:54 +0600

    constant as define()

--- index.php
+++ index.php
@@ -7,34 +7,71 @@
 </head>
 <body>
 
-<?php 
-$x = 5; // integer
-$y = 10.32; // float or doshomik
+<?php
+$variable = 10;
+echo $variable;
 echo "<br>";
-var_dump($x); // integer
+$variable = 20;
 echo "<br>";
-var_dump($y); // float
+echo $variable;
+echo "<br>";
+// as you see $ common variable can be changed without any error but
+define('a', 15);
+echo ""; // this will work cz you can never change there output
+echo "<br>";
+define('a', 20);
+echo a; // here comes error
+// constant fruit already defined so you can not change the output
+// you can only read!
+// if you comment any of the folloing const fruit the error will be gone
 ?>
 
 <?php 
-echo (pi()); // you will get PI number
 echo "<br>";
-echo (min(100,15,30,15,40));
-echo "<br>";
-echo (max(50,70,80,90,100));
-echo "<br>";
-echo (abs(-10)); // always return positive
-echo "<br>";
-echo (sqrt(16)); // output will be 4
-echo "<br>";
-// flout ke jodi purno number eh convert korty hoy taholu
-echo (round(35.254545));
-echo "<br>";
-// random number er jonno
-echo (rand(1, 6));
+//array
+define('cars', [
+    'BMW', 'Toyota', 'Ferari'
+]);
+
+echo (cars[0]);
 ?>
 
+<!-- $variables are not global but constant variables are global -->
 
+<?php 
+$name = "akash";
+
+function functionName(){
+    echo($name);
+}
+
+functionName(); // Undefined variable $name
+// cz $$name is not gloabl !its only works in local
+?>
+
+<?php 
+
+function functionNameTwo(){
+    $name = "akash";
+    echo($name);
+}
+
+functionNameTwo(); // now the function will give us the output
+//not $name is local variable so it will work.
+?>
+
+<br>
+<?php 
+define('age', 10);
+
+function ageNumber(){
+    echo(age);
+}
+
+ageNumber(); // age is a constant variable and global variable 
+// so it will work without any issues
+
+?>
 <body>
 </body>
 </html>
